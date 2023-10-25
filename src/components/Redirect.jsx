7c1d7a0f84dbe692f16260cc8d7ab80a2c6b26dd
developersharif <developersharif@yahoo.com>
2023-10-25 19:56:22 +0600

    route updated

--- src/components/Redirect.jsx
+++ src/components/Redirect.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 function Redirect() {
     const navigate = useNavigate();
     useEffect(()=>{
-        navigate("/")
+        navigate("/redirected")
     });
     return (<>
     {/* <Navigate to="/test"></Navigate> */}
