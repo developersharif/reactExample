84f59a845471d8faf1e743138b115a068d88bef9
developersharif <developersharif@yahoo.com>
2023-10-25 22:15:14 +0600

    syntax-highlighter added

--- src/components/CodeExample.jsx
+++ src/components/CodeExample.jsx
@@ -1,5 +1,11 @@
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 function CodeExample({ code }) {
-  return <>{code}</>;
+  return(
+    <SyntaxHighlighter language="javascript" style={docco}>
+      {code}
+    </SyntaxHighlighter>
+  );
 }
 
 export default CodeExample;
--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,7 +1,9 @@
 function Description({children}) {
   return(<>
   <br />
+  <div style={{border:'1px solid gray',padding:'10px',margin:'5px'}}>
   {children}
+  </div>
   </>)
 }
 
