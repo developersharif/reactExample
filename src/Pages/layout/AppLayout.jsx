82d420c70eee6aaa0dd37e4a16964a64e313db60
developersharif <developersharif@yahoo.com>
2023-10-25 20:00:14 +0600

    Default Layout created

--- src/Pages/layout/AppLayout.jsx
+++ src/Pages/layout/AppLayout.jsx
@@ -20,12 +20,18 @@ const AppLayout = ({title="Default Title",children}) => {
         <div>
         Global State: {data.data.data.user.name} { data.data.data.user.email }
       <h2> Routes:</h2>
+      <nav style={{display:'flex',flexWrap: 'wrap',justifyContent: 'space-evenly'}}>
       <Link to="/">Home</Link> <br />
-      <Link to="/test">Loader APi Example</Link> <br />
+      <Link to="/usestate">useState Example</Link> <br />
+      <Link to="/memo">Memo Example</Link> <br />
+      <Link to="/reducer">useReducer Example</Link> <br />
+      <Link to="/context">Context API</Link> <br />
+      <Link to="/loader">Route Loader</Link> <br />
       <Link to="/redirect">Redirect</Link> <br />
       <Link to="/update/hello-world">Parameter</Link> <br />
       <Link to="/datatable" replace={true}>Data Table</Link> <br />
-            <br />
+      </nav>
+            <hr />
             {children}
         </div>
     );
